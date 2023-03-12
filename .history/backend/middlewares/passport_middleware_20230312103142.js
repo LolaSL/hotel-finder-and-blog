d2939dcf7a8a2b87baeb41f2969c78e282a7025d
LolaSL <lolashepota@gmail.com>
2023-03-12 11:06:52 +0200

    Modify files:   modified:   package.json

---
+++ .history/backend/middlewares/passport_middleware_20230312103142.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const passport = require('passport');
+var JwtStrategy = require('passport-jwt').Strategy;
+
+const SECRET = process.env.SECRET;
+const db = require('../db');
+
+const  ExtractJwt = function (req) {
+    let token = null
+    if (req &&  request.cookies.jwt) token = req.cookies['token']
+    return token
+};
+    
+const options = {
+    secretOrKey: SECRET,
+    jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('jwt')
+};
+
+passport.use(
+    new Strategy(options, async ({ id }, done) => {
+        try {
+            const { rows } = await db.query(
+                'SELECT user_id, email FROM users WHERE user_id = $1',
+                [id]
+            )
+
+            if (!rows.length) {
+                throw new Error('401 not authorized')
+            }
+
+            let user = { id: rows[0].user_id, email: rows[0].email }
+
+            return done(null, user)
+        } catch (error) {
+            console.log(error.message)
+            done(null, false)
+        }
+    })
+);
